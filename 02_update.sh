19b3e6642b6b4b15be71da3052230114772c4836
y-sk <y-sk@github.com>
2019-03-06 11:41:17 +0900

    init

---
+++ 02_update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+yyyymmdd=`date "+%Y%m%d"`
+
+f=${yyyymmdd}.md
+
+[ ! -e ${f} ] && echo "not found ${f}" && exit 1
+
+modified=`git status ${f} | grep modified`
+
+[ "${modified}" == "" ] && echo "no change ${f}" && exit 1
+
+echo ${modified}
+
+git pull
+
+git add ${f}
+
+git commit -m "update ${f}" 
+
+git push
+
