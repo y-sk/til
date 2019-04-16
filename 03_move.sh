1a5319e3ee2507409bc3fe89d0371cb839431e73
y-sk <y-sk@users.noreply.github.com>
2019-04-16 10:14:31 +0900

    update 03_move.sh

--- 03_move.sh
+++ 03_move.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+
+
 yyyymmdd=`date "+%Y%m%d"`
 
 yyyymm01=`date "+%Y%m01"`
 
-yyyymm=`date -j -v-1m -f "%Y%m%d" "${yyyymm01}" "+%Y%m"`
+yyyymm=`date "+%Y%m"`
+
+[ $# -eq 1 ] && yyyymm=`date -j -v-1m -f "%Y%m%d" "${yyyymm01}" "+%Y%m"`
 
 d=${yyyymm}
 
 branch_name=develop/move_${yyyymmdd}
-
-[ ! -e ${d} ] && mkdir ${d}
 
 # TODO 作成済みのブランチ確認とチェックアウト済みのブランチ確認
 
@@ -20,6 +22,8 @@ git checkout -b ${branch_name} origin/master
 
 git push -u origin ${branch_name}
 
+[ ! -e ${d} ] && mkdir ${d}
+
 git mv ${yyyymm}*.md ${d}
 
 git add ${d}/*
@@ -27,5 +31,4 @@ git add ${d}/*
 git commit -m "move ${yyyymm}*.md to ${d}" 
 
 git push
-
 
