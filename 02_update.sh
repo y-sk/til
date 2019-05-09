c6e172559ec5d48fb69ef4835d3fbb502a262b74
y-sk <y-sk@users.noreply.github.com>
2019-05-09 10:31:52 +0900

    特定のファイルを更新できるように修正

--- 02_update.sh
+++ 02_update.sh
@@ -4,6 +4,8 @@ yyyymmdd=`date "+%Y%m%d"`
 
 f=${yyyymmdd}.md
 
+[ "$1" != "" ] && f=${1}.md
+
 [ ! -e ${f} ] && echo "not found ${f}" && exit 1
 
 modified=`git status ${f} | grep modified`
@@ -17,6 +19,3 @@ git pull
 git add ${f}
 
 git commit -m "update ${f}" 
-
-[ "${1}" == "1" ] && git push
-
