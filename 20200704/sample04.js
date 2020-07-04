31cc0f91c8783f53862954498b23a2a7a46bbaae
y-sk <y-sk@users.noreply.github.com>
2020-07-04 11:56:27 -0400

    update 20200704.md

---
+++ 20200704/sample04.js
@@ -0,0 +1,24 @@
+function main() {
+  const list = [1, 2, 3];
+  for ( n of list ) {
+    let funcName = ('func%s', n);
+    func(funcName, Math.random() * 1000);
+  }
+}
+
+async function func(funcName, timeout) {
+  console.log('Start %s %s', funcName, timeout);
+  await sleep(timeout);
+  console.log('End   %s %s', funcName, timeout);
+  return funcName;
+}
+
+async function sleep(timeout) {
+  return new Promise( resolve => {
+    setTimeout(() => {
+      resolve();
+    }, timeout);
+  });
+}
+
+main();
