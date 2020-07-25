97799516b70a73fed73bb11f4f134594db98bdb9
y-sk <y-sk@users.noreply.github.com>
2020-07-25 13:22:34 -0400

    update 20200725

--- 20200725/lambda/index.js
+++ 20200725/lambda/index.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
-  console.log("ENV:",     JSON.stringify(process.env))
-  console.log("EVENT:",   JSON.stringify(event)      )
-  console.log("CONTEXT:", JSON.stringify(context)    )
+  console.log("ENV:",     JSON.stringify(process.env, null, 2))
+  console.log("EVENT:",   JSON.stringify(event,       null, 2))
+  console.log("CONTEXT:", JSON.stringify(context,     null, 2))
   return context.logStreamName
 }
